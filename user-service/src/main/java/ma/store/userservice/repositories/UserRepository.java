b9d741b15c9d0411a0d2929c7524f1a97233666e
Achrafvin <achraf.derkaoui@emsi-edu.ma>
2023-12-09 00:52:45 +0100

    RemoveRepo

--- user-service/src/main/java/ma/store/userservice/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package ma.store.userservice.repositories;
-
-import ma.store.userservice.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
