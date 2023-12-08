66797679ab39b70006a33d7cf5317e84be4f0eef
adyatar <119011457+adyatar@users.noreply.github.com>
2023-12-08 02:21:57 +0100

    first-derke-commit

---
+++ src/main/java/com/example/adyaderka/AdyaDerkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.adyaderka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdyaDerkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdyaDerkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/adyaderka/AdyaDerkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adyaderka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdyaDerkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
