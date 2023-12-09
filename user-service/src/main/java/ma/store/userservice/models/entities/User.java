891cb9c36a703f1a6a900c3889b7a1357a392dcf
Achrafvin <achraf.derkaoui@emsi-edu.ma>
2023-12-09 03:50:56 +0100

    AffUSerMicro

--- user-service/src/main/java/ma/store/userservice/models/entities/User.java
+++ user-service/src/main/java/ma/store/userservice/models/entities/User.java
@@ -1,14 +1,12 @@
 package ma.store.userservice.models.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
 @AllArgsConstructor @NoArgsConstructor @Getter @Setter @ToString
 @Builder
+@Table(name = "db_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
