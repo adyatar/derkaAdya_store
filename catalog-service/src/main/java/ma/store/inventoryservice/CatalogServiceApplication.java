2ccd8869a955aa73fd4dc30aa098774af36b22e0
Achrafvin <achraf.derkaoui@emsi-edu.ma>
2023-12-23 00:06:44 +0100

    AchUpdate

--- catalog-service/src/main/java/ma/store/inventoryservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/ma/store/inventoryservice/CatalogServiceApplication.java
@@ -19,40 +19,82 @@ public class CatalogServiceApplication {
 	CommandLineRunner start(CategoryRepository categoryRepository, ProductRepository productRepository)
 	{
 		return args -> {
-			Category category1 = Category.builder()
-					.name_cat("TV")
-					.img_name("tv_image.jpg")
+			Category phoneCat = Category.builder()
+					.name_cat("Phone")
+					.img_name("assets/images/apple.png")
 					.build();
 
-			Category category2 = Category.builder()
-					.name_cat("Phone")
-					.img_name("phone_image.jpg")
+			Category watchCat = Category.builder()
+					.name_cat("Watch")
+					.img_name("assets/images/watch.png")
+					.build();
+			Category laptop = Category.builder()
+					.name_cat("Laptop")
+					.img_name("assets/images/watch.png")
+					.build();
+			Category headPhones = Category.builder()
+					.name_cat("HeadPhones")
+					.img_name("assets/images/watch.png")
 					.build();
 
 			// Save categories
-			categoryRepository.save(category1);
-			categoryRepository.save(category2);
+			categoryRepository.save(phoneCat);
+			categoryRepository.save(watchCat);
+			categoryRepository.save(laptop);
+			categoryRepository.save(headPhones);
 
 			// Create products and associate them with categories using Lombok Builder
-			Product product1 = Product.builder()
-					.name_prod("Smart TV")
-					.desc_prod("4K Ultra HD Smart TV")
-					.price(1200.00)
+			Product iphone = Product.builder()
+					.name_prod("IPhone 15 Pro Max")
+					.desc_prod("IPhone 15 Pro Max with 512 GB")
+					.price(1099.00)
 					.qte_prod(50)
-					.category(category1)
+					.image_prod("apple.png")
+					.category(phoneCat)
 					.build();
 
-			Product product2 = Product.builder()
-					.name_prod("Smartphone")
-					.desc_prod("Latest model smartphone")
-					.price(800.00)
-					.qte_prod(100)
-					.category(category1)
+			Product watch = Product.builder()
+					.name_prod("Apple Watch Serie 9")
+					.desc_prod("Latest Apple Watch model")
+					.price(300.00)
+					.qte_prod(50)
+					.image_prod("huawei.png")
+					.category(watchCat)
+					.build();
+
+			Product macbook = Product.builder()
+					.name_prod("Macbook Pro 2021")
+					.desc_prod("Latest Macbook Pro model")
+					.price(3000.00)
+					.qte_prod(50)
+					.image_prod("beats.jpg")
+					.category(laptop)
+					.build();
+
+			Product airpods = Product.builder()
+					.name_prod("Airpods Pro")
+					.desc_prod("Latest Airpods model")
+					.price(300.00)
+					.qte_prod(50)
+					.image_prod("power.png")
+					.category(headPhones)
+					.build();
+
+			Product airpodsMax = Product.builder()
+					.name_prod("Airpods Max")
+					.desc_prod("Latest Airpods Max model")
+					.price(300.00)
+					.qte_prod(50)
+					.image_prod("power.png")
+					.category(headPhones)
 					.build();
 
 			// Save products
-			productRepository.save(product1);
-			productRepository.save(product2);
+			productRepository.save(watch);
+			productRepository.save(iphone);
+            productRepository.save(macbook);
+            productRepository.save(airpods);
+            productRepository.save(airpodsMax);
 		};
 	}
 
