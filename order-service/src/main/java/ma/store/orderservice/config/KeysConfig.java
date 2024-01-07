29729ca1d7d671012203db3425e1188161883066
Achrafvin <achraf.derkaoui@emsi-edu.ma>
2024-01-07 23:56:42 +0100

    Ach

---
+++ order-service/src/main/java/ma/store/orderservice/config/KeysConfig.java
@@ -0,0 +1,9 @@
+package ma.store.orderservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.interfaces.RSAPublicKey;
+
+@ConfigurationProperties(prefix = "rsa")
+public record KeysConfig(RSAPublicKey publicKey) {
+}
---
+++ order-service/src/main/java/ma/store/orderservice/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package ma.store.orderservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final KeysConfig keysConfig;
+
+    public SecurityConfig(KeysConfig keysConfig) {
+        this.keysConfig = keysConfig;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .authorizeHttpRequests(auth->auth.requestMatchers(HttpMethod.GET,"/h2-console/**").permitAll())
+                .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth->oauth.jwt(Customizer.withDefaults()))
+                .sessionManagement(sess->sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+    @Bean
+    JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withPublicKey(keysConfig.publicKey()).build();
+    }
+}
